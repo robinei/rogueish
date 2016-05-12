ff27da70820440d5a3fa964793256f701595ca54
Robin Eidissen <robinei@gmail.com>
2016-05-12 21:05:03 +0200

    use full 32 bits for colors

--- src/color.ts
+++ src/color.ts
@@ -4,11 +4,8 @@
 export type Color = number;
 
 
-export const makeColor = (r: number, g: number, b: number, a?: number): Color => {
-    if (a === undefined) {
-        a = 255;
-    }
-    return (r << 23) | (g << 15) | (b << 7) | (a >>> 1);
+export const makeColor = (r: number, g: number, b: number, a: number = 255): Color => {
+    return (((r << 24) >>> 0) | ((g << 16) | (b << 8) | a)) >>> 0;
 };
 
 
@@ -36,7 +33,7 @@ export const colors = {
 export const parseColor = (str: string): Color => {
     if (str.charAt(0) == '#') {
         if (str.length === 7) {
-            return (parseInt(str.substr(1), 16) << 7) | 127;
+            return (((parseInt(str.substr(1), 16) << 8) >>> 0) | 255) >>> 0;
         }
         throw new Error('expected 6 digit hex value: ' + str);
     }
@@ -49,9 +46,9 @@ export const parseColor = (str: string): Color => {
 
 
 export const toStringColor = (color: Color): string => {
-    const a = color & 127;
-    if (a == 127) {
-        const str = (color >>> 7).toString(16);
+    const a = color & 255;
+    if (a == 255) {
+        const str = (color >>> 8).toString(16);
         switch (str.length) {
         case 6: return '#' + str;
         case 5: return '#0' + str;
@@ -63,39 +60,39 @@ export const toStringColor = (color: Color): string => {
         }
         throw new Error('this should not happen');
     }
-    const r = color >>> 23;
-    const g = (color >>> 15) & 255;
-    const b = (color >>> 7) & 255;
-    return 'rgba(' + r + ',' + g + ',' + b + ',' + (a/127).toFixed(3) + ')';
+    const r = color >>> 24;
+    const g = (color >>> 16) & 255;
+    const b = (color >>> 8) & 255;
+    const aFixed = (a/255).toFixed(3);
+    return `rgba(${r},${g},${b},${aFixed})`;
 }
 
 
 export const scaleColor = (color: Color, factor: number) => {
-    const r = Math.floor((color >>> 23) * factor) & 255;
-    const g = Math.floor(((color >>> 15) & 255) * factor) & 255;
-    const b = Math.floor(((color >>> 7) & 255) * factor) & 255;
-    const a = color & 127;
-    return (r << 23) | (g << 15) | (b << 7) | a;
+    const r = Math.floor((color >>> 24) * factor) & 255;
+    const g = Math.floor(((color >>> 16) & 255) * factor) & 255;
+    const b = Math.floor(((color >>> 8) & 255) * factor) & 255;
+    return makeColor(r, g, b, color & 255);
 }
 
 
 export const blendColors = (c0: Color, c1: Color, t: number) => {
-    var c0r = (c0 >>> 23) / 255;
-    var c0g = ((c0 >>> 15) & 255) / 255;
-    var c0b = ((c0 >>> 7) & 255) / 255;
-    var c0a = (c0 & 127) / 127;
+    var c0r = (c0 >>> 24) / 255;
+    var c0g = ((c0 >>> 16) & 255) / 255;
+    var c0b = ((c0 >>> 8) & 255) / 255;
+    var c0a = (c0 & 255) / 255;
     
-    var c1r = (c1 >>> 23) / 255;
-    var c1g = ((c1 >>> 15) & 255) / 255;
-    var c1b = ((c1 >>> 7) & 255) / 255;
-    var c1a = (c1 & 127) / 127;
+    var c1r = (c1 >>> 24) / 255;
+    var c1g = ((c1 >>> 16) & 255) / 255;
+    var c1b = ((c1 >>> 8) & 255) / 255;
+    var c1a = (c1 & 255) / 255;
     
-    var r = Math.floor((t * c1r + (1 - t) * c0r) * 255) & 255;
-    var g = Math.floor((t * c1g + (1 - t) * c0g) * 255) & 255;
-    var b = Math.floor((t * c1b + (1 - t) * c0b) * 255) & 255;
-    var a = Math.floor((t * c1a + (1 - t) * c0a) * 127) & 127;
+    var r = Math.floor((t * c1r + (1 - t) * c0r) * 255);
+    var g = Math.floor((t * c1g + (1 - t) * c0g) * 255);
+    var b = Math.floor((t * c1b + (1 - t) * c0b) * 255);
+    var a = Math.floor((t * c1a + (1 - t) * c0a) * 255);
     
-    return (r << 23) | (g << 15) | (b << 7) | a;
+    return makeColor(r, g, b, a);
 }
 
 
